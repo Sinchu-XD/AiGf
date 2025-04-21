97eab24cd8a363b0160c49b9d6d346f91035b786
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-04-21 20:57:37 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -24,3 +24,6 @@ async def main():
 if __name__ == "__main__":
     try:
         asyncio.get_event_loop().run_until_complete(main())
+    except KeyboardInterrupt:
+        print("🛑 Bot stopped manually.")
+
